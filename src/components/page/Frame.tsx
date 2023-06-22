56a8f68a81c32e8bdc67690b19a55b7e76e9b301
okmtyuta <okmtyuta@gmail.com>
2023-06-22 16:16:04 +0900

    a

--- src/components/page/Frame.tsx
+++ src/components/page/Frame.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react'
 import styles from './frame.module.scss'
-import { Menu } from '../amatelas/navigation/menu/Menu'
+// import { Menu } from '../amatelas/navigation/menu/Menu'
 import { Space } from '../layout/Space'
 import { Highlight } from '../amatelas/display/highlight/Highlight'
 import { Header } from '../amatelas/base/header/Header'
@@ -10,39 +10,39 @@ interface FrameProps {
   pageId: number
 }
 
-const menuItems = [
-  {
-    id: 1,
-    label: 'Introduction',
-    to: '/',
-    active: false
-  },
-  {
-    id: 2,
-    label: 'Design Rule',
-    to: 'design-rule',
-    active: false
-  }
-]
+// const menuItems = [
+//   {
+//     id: 1,
+//     label: 'Introduction',
+//     to: '/',
+//     active: false
+//   },
+//   {
+//     id: 2,
+//     label: 'Design Rule',
+//     to: 'design-rule',
+//     active: false
+//   }
+// ]
 
 export const Frame = (props: FrameProps) => {
-  const activatedMenuItems = menuItems.map((menuItem) => {
-    if (menuItem.id === props.pageId) {
-      return {
-        ...menuItem,
-        active: true
-      }
-    }
+  // const activatedMenuItems = menuItems.map((menuItem) => {
+  //   if (menuItem.id === props.pageId) {
+  //     return {
+  //       ...menuItem,
+  //       active: true
+  //     }
+  //   }
 
-    return menuItem
-  })
+  //   return menuItem
+  // })
   return (
     <div className={styles.frame}>
       <div className={styles.header}>
         <Header></Header>
       </div>
       <div className={`${styles.menu} ${styles.hide}`}>
-        <Menu menuItems={activatedMenuItems} />
+        {/* <Menu menuItems={activatedMenuItems} /> */}
       </div>
       <div className={styles.main}>
         <div>{props.children}</div>
